bd32c3733b8a65e53f39615ea9e1d1d8327922d3
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-10-02 20:36:45 +0530

    in question 1:inside execution class: process done and output printed and question1 completed

--- src/question1/Execution.java
+++ src/question1/Execution.java
@@ -6,5 +6,14 @@ public class Execution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().trim();
+        System.out.print("\nIndex: ");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.indexOf(str.charAt(i)) + "\t");
+        }
+        System.out.print("\nChars: ");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + "\t");
+        }
+
     }
 }
