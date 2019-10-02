7a9c90313c9214cef4151a29645453bc7829ff92
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-10-02 20:47:20 +0530

    execution class for question 3 is added

--- src/question2/Execution.java
+++ src/question2/Execution.java
@@ -6,5 +6,7 @@ public class Execution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().trim();
+        int n = sc.nextInt();
+        System.out.println(str.substring(n));
     }
 }
