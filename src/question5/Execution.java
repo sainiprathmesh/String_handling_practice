5ac99c3ccfbaf736620888b605bab1835d196d5e
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-10-05 10:47:35 +0530

    question 5 completed

--- src/question5/Execution.java
+++ src/question5/Execution.java
@@ -1,4 +1,16 @@
 package question5;
 
+import java.util.Scanner;
+
 public class Execution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine().trim();
+        String target = sc.nextLine().trim();
+        String replacement = sc.nextLine().trim();
+        CharSequence t = target;
+        CharSequence r = replacement;
+        String s2 = st.replace(t, r);
+        System.out.println(s2);
+    }
 }
