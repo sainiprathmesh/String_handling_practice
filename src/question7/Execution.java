9749768e1a5afb89fdeedef662c72dd5567f7a94
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-10-05 10:49:50 +0530

    question 7 completed

--- src/question7/Execution.java
+++ src/question7/Execution.java
@@ -1,4 +1,15 @@
 package question7;
 
+import java.util.Scanner;
+
 public class Execution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int l = s.length();
+        long a = 0;
+        for (int i = 0; i < l; i++)
+            a += 0 + s.charAt((i));
+        System.out.println("total weight=" + a);
+    }
 }
