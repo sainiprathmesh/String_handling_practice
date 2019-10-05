ef44654adb74ddf3d93de8ae602b707ccd6fc9b7
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-10-05 10:48:42 +0530

    question 6 completed

--- src/question6/Execution.java
+++ src/question6/Execution.java
@@ -1,4 +1,11 @@
 package question6;
 
+import java.util.Scanner;
+
 public class Execution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        System.out.println(s.toUpperCase());
+    }
 }
